e96eac890c227b7938c3cd4bff306ed0699b2bb3
Ivan Danilkin <idanilkin26@gmail.com>
2020-10-11 19:36:31 +0300

    сделали чередование крестиков и ноликов

--- js.js
+++ js.js
@@ -1,10 +1,22 @@
 "use strict";
 let buttons = document.querySelector("table");
+let step_count = 1;
 
 buttons.onclick = function(event) {
 	let target = event.target;
 	if (target.tagName!="TD") {
 		return;
 	}
-	target.innerHTML = "X";
+	if(target.innerHTML != "") {
+		return;
+	}
+	if(step_count % 2 == 1){
+		target.innerHTML = "X";
+	} else {
+		target.innerHTML = "O";
+	}
+	// варианты увеличения переменной на 1 
+	// step_count = step_count + 1;
+	// step_count += 1;
+	step_count++;
 };
