8a395eb10a8dd8f2aec1991a0bff65b892d8de43
Ivan Danilkin <idanilkin26@gmail.com>
2020-11-15 20:29:59 +0300

    Сделал победу

--- js.js
+++ js.js
@@ -1,16 +1,29 @@
 "use strict";
 let buttons = document.querySelector("table");
 let step_count = 1;
-
-buttons.onclick = function(event) {
+let all_td = document.querySelectorAll("td");
+let win_combination = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+];
+buttons.onclick = function (event) {
 	let target = event.target;
-	if (target.tagName!="TD") {
+	/* если элемент на который нажали не является яйчейкой, то выходим */
+	if (target.tagName != "TD") {
 		return;
 	}
-	if(target.innerHTML != "") {
+	/*если элемент на который нажали не пустой, то выходим*/
+	if (target.innerHTML != "") {
 		return;
 	}
-	if(step_count % 2 == 1){
+	/*если число разделили на 2 и получили остаток 1, то в нажатый элемент вставить крестик, иначе нолик*/
+	if (step_count % 2 == 1) {
 		target.innerHTML = "X";
 	} else {
 		target.innerHTML = "O";
@@ -19,4 +32,17 @@ buttons.onclick = function(event) {
 	// step_count = step_count + 1;
 	// step_count += 1;
 	step_count++;
+	check();
 };
+
+function check() {
+	for (let i = 0; i < win_combination.length; i++) {
+		if (all_td[win_combination[i][0]].innerHTML != "" &&
+			all_td[win_combination[i][0]].innerHTML == all_td[win_combination[i][1]].innerHTML &&
+			all_td[win_combination[i][0]].innerHTML == all_td[win_combination[i][2]].innerHTML) {
+			alert("Победа!");
+		}
+
+	}
+
+}
